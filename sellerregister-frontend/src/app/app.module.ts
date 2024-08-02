1c3c868dfab5790e2f56d8a014b9e0c8bddff8a2
Pedro Vinincius <viniciuspedro0905@gmail.com>
2024-08-02 14:17:33 -0300

    closes #1 - Initial Commit

---
+++ sellerregister-frontend/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { SellersComponent } from './components/sellers/sellers.component';
+import { SellerFormComponent } from './components/seller-form/seller-form.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    SellersComponent,
+    SellerFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ sellerregister-frontend/src/app/components/seller-form/seller-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-seller-form',
+  templateUrl: './seller-form.component.html',
+  styleUrl: './seller-form.component.css'
+})
+export class SellerFormComponent {
+
+}
