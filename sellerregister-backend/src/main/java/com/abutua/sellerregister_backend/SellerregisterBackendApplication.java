b374bc61ceabcf28c9aac932ea9a80303b8af4ab
Pedro Vinincius <viniciuspedro0905@gmail.com>
2024-08-02 14:19:09 -0300

    added the backend

---
+++ sellerregister-backend/src/main/java/com/abutua/sellerregister_backend/SellerregisterBackendApplication.java
@@ -0,0 +1,13 @@
+package com.abutua.sellerregister_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SellerregisterBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SellerregisterBackendApplication.class, args);
+	}
+
+}
---
+++ sellerregister-backend/src/test/java/com/abutua/sellerregister_backend/SellerregisterBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abutua.sellerregister_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SellerregisterBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
