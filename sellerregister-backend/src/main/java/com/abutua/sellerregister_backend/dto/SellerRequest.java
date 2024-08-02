b1db434772d39c8b87f491a4cf797dbcc7706028
Pedro Vinincius <viniciuspedro0905@gmail.com>
2024-08-02 14:56:31 -0300

    closes #3 - Created the Seller DTOs

---
+++ sellerregister-backend/src/main/java/com/abutua/sellerregister_backend/dto/SellerRequest.java
@@ -0,0 +1,73 @@
+package com.abutua.sellerregister_backend.dto;
+
+import com.abutua.sellerregister_backend.models.Seller;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+
+public class SellerRequest {
+    
+    @NotBlank(message = "Name can't be null")
+    @Size(min = 5, max = 255, message = "Name must have min = 5 and max = 255")
+    private String name;
+
+    @NotNull(message = "Seller must have a salary")
+    @Positive(message = "Salary must be higher than zero")
+    private Double salary;
+
+    @Min(value = 0, message = "The Bonus min is 0%")
+    @Max(value = 100, message = "The Bonus max is 100%")
+    private Double bonus;
+
+    @NotNull(message = "Seller must have a gender")
+    @Min(value = 0, message = "0 = Male and 1 = Female")
+    @Min(value = 1, message = "0 = Male and 1 = Female")
+    private Integer gender;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+
+    public Double getBonus() {
+        return bonus;
+    }
+
+    public void setBonus(Double bonus) {
+        this.bonus = bonus;
+    }
+
+    public Integer getGender() {
+        return gender;
+    }
+
+    public void setGender(Integer gender) {
+        this.gender = gender;
+    }
+
+    public Seller toEntity(){
+        Seller seller = new Seller();
+
+        seller.setName(name);
+        seller.setSalary(salary);
+        seller.setBonus(bonus);
+        seller.setGender(gender);
+
+        return seller;
+    }
+}
---
+++ sellerregister-backend/src/main/java/com/abutua/sellerregister_backend/dto/SellerResponse.java
@@ -0,0 +1,63 @@
+package com.abutua.sellerregister_backend.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+@JsonInclude(Include.NON_NULL)
+public class SellerResponse {
+    private Long id;
+    private String name;
+    private Double salary;
+    private Double bonus;
+    private Integer gender;
+
+    public SellerResponse(Long id, String name, Double salary, Double bonus, Integer gender) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+        this.bonus = bonus;
+        this.gender = gender;
+    }
+
+    public SellerResponse() { }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+
+    public Double getBonus() {
+        return bonus;
+    }
+
+    public void setBonus(Double bonus) {
+        this.bonus = bonus;
+    }
+
+    public Integer getGender() {
+        return gender;
+    }
+
+    public void setGender(Integer gender) {
+        this.gender = gender;
+    } 
+}
